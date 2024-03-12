b42b23b44a0ba67c055c64a80b6817817c2477ba
Matheus Oliveira <143137475+theusontech@users.noreply.github.com>
2024-03-12 01:05:55 +0000

    Sua mensagem de commit aqui

--- Hello.py
+++ Hello.py
@@ -1,51 +1,12 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
 
-LOGGER = get_logger(__name__)
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets/ranking.csv", index_col=0)
+    st.session_state["data"] = df_data
 
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+st.write("# Ranking de membros RedTails:crossed_swords:")
+st.write("""
+Este projeto é da guild RedTails do Ragnarok Origin, que busca atualizar e mostrar o ranking dos jogadores dentro da guild. Futuramente, serão implementadas novas atualizações.
+""")
+st.sidebar.write("Esse é um prjeto para a Guild Redtails para visualização de membros, ranking, level e contribuição")
---
+++ pages/Classes.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+
+# Carrega os dados do arquivo CSV
+@st.cache
+def load_data():
+    # Substitua o caminho abaixo pelo caminho do seu arquivo CSV
+    df = pd.read_csv("ranking.csv")
+    return df
+
+# Verifica se os dados foram carregados na session state
+if "data" not in st.session_state:
+    st.error("Os dados ainda não foram carregados. Por favor, execute o arquivo home.py primeiro.")
+    st.stop()
+
+# Carrega os dados do session state
+df_data = st.session_state["data"]
+
+# Verifica se a coluna "Classe" existe nos dados
+if "Classe" not in df_data.columns:
+    st.error("A coluna 'Classe' não foi encontrada nos dados. Por favor, verifique se os dados foram carregados corretamente.")
+    st.stop()
+
+# Acessa a coluna "Classe" e obtém os valores únicos
+classes = ["Todas as Classes"] + df_data["Classe"].unique().tolist()
+
+# Mostra um seletor de caixa de seleção na barra lateral para escolher a classe
+selected_class = st.sidebar.selectbox("Classes", classes)
+
+# Se "Todas as Classes" não foi selecionada
+if selected_class != "Todas as Classes":
+    # Filtra os dados para mostrar apenas os jogadores da classe selecionada
+    df_players = df_data[df_data["Classe"] == selected_class]
+    # Exibe todos os jogadores da classe selecionada
+    st.write(df_players)
+else:
+    # Exibe todos os jogadores de todas as classes
+    st.write(df_data)
